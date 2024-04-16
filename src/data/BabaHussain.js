b0c95c8daa696a04ca0b8e26e2753d2004c34c78
Baba Hussain <bedadaluribabahussain2000@gmail.com>
2024-04-16 13:27:43 +0530

    My project

--- src/data/BabaHussain.js
+++ src/data/BabaHussain.js
@@ -2,7 +2,7 @@ import MailIcon from "@material-ui/icons/Mail";
 import PhoneIcon from "@material-ui/icons/Phone";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TurnedInIcon from "@material-ui/icons/TurnedIn";
-import Image from "../images/Baba.jpeg";
+import Image from "../images/Baba.png";
 
 const path = "BabaHussain";
 const contact = [
@@ -16,7 +16,7 @@ const contact = [
   },
   {
     icon: <LinkedInIcon />,
-    text: "https://www.linkedin.com/in/baba-hussain-bedadaluri-16370717a",
+    text: "https://www.linkedin.com/in/babahussain",
     val: 2,
   },
 ];
@@ -31,11 +31,11 @@ const awards = [
 const sideBarContents = [
   {
     title: "Professional Summary-Proficient",
-    list: "React JS, JavaScript, Redux, HTML5, CSS, Responsive Design, Bootstrap, Azure App Services",
+    list: "  HTML5, CSS, Tailwind CSS, JavaScript, React JS, Next JS, Redux, Responsive Design, Azure App Services",
   },
   {
     title: "Professional Summary-Exposure",
-    list: "PL-SQL, SQL, Java, GIT",
+    list: "Node JS, Express, MongoDB, GIT",
   },
   {
     title: "Interests",
@@ -53,41 +53,35 @@ const sideBarContents = [
 
 const name = "Baba Hussain Bedadaluri";
 const designation = "Software Engineer";
-const proSummary = `1 year of experience in building Responsive web applications.
-Proficient in writing high-quality code and application interface code via Javascript and ReactJS
-Proven track record of building reusable components.
-Experienced in Full-stack development.
-Excellent analytical, problem-solving skills and self-motivated.
-Ability to adopt new technologies swiftly and excellent code debugging skills.`;
+const proSummary = `Over 2.5 years of experience developing responsive web applications, adept at translating designs into high-quality code using JavaScript and ReactJS.
+Proven track record in building reusable components and working in Scrum teams.
+Skilled in full-stack development, with strong code debugging and problem-solving abilities.
+Quick learner with a self-motivated approach, capable of swiftly adopting new technologies.
+Excellent analytical skills, coupled with a proactive attitude towards problem-solving.
+Effective communicator, able to collaborate seamlessly with cross-functional teams to achieve project objectives.`;
 
 const workExperiance = [
   {
     designation: "Software Development Consultant | Front End Developer",
     duration: "Tata Consultancy Services| March 2022 - present",
-    contribution: `Developed Staff login pages in WHS website with React
-            Developed highly reusable responsive components.
-            Created reusable Address Typeahead component which is used across multiple pages on WHS's app.
-            Integrated Redux throughout the Application
-            Integrated all APIS using Redux-thunk.`,
+    contribution: `Engineered staff login pages within the Biogen Healthcare System (BHS) website using React.
+    Developed a suite of highly reusable and responsive components to enhance website functionality.
+    Designed and implemented a reusable Address component, deployed across multiple pages within the Biogen Healthcare System (BHS) application.
+    Integrated Redux seamlessly throughout the application to manage state efficiently and ensure scalability.
+    Utilized Redux-thunk to seamlessly integrate all APIs, optimizing data flow and enhancing application performance.`,
   },
 ];
 
 const personalProjects = [
   {
-    title: "Weather Forecast",
-    info: "A responsive weather forecast website is built with react js. It can show real-time weather reports of any city in the world and five days forecast.",
-    web_link:
-      "https://babahussainbedadaluri.github.io/new-weather-forecast-app/",
+    title: "Natours",
+    info: "A fully integrated website, enabling users to register, authenticate, explore tour offerings, conduct transactions via debit or credit cards, and access interactive maps for tour destinations. This platform was constructed utilizing React.js, Express, Node.js, and MongoDB.",
+    web_link: "https://natours-79sq.onrender.com/",
   },
   {
-    title: "Bank of Baroda",
-    info: "An interactive Dummy Bank website, where account holders can log in, make payments, history of transactions, and can dismiss the account. (User: js Pin: 1111)",
-    web_link: "https://babahussainbedadaluri.github.io/Bank-of-Baroda/",
-  },
-  {
-    title: "Dice Game",
-    info: "An entertainment Dice Game, where two players can roll a dice based on their turn. Players who first reach the score of 100 will win.",
-    web_link: " https://babahussainbedadaluri.github.io/Dices-game/",
+    title: "YouTube Clone",
+    info: "I spearheaded the development of a YouTube replica website, leveraging frontend technologies such as React.js, Tailwind CSS, and Rest APIs. The platform features a homepage categorizing content into sections like entertainment, sports, news, and trending sessions. Additionally, it incorporates a search functionality allowing users to explore available YouTube videos seamlessly.",
+    web_link: "https://funny-kitsune-1d688b.netlify.app/",
   },
 ];
 
